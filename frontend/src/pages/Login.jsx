92a9d5c5d3b8f16cdd3daf6aa68fb20aad774979
unknown <asphyxgrg@gmail.com>
2024-09-12 23:07:52 +0545

    login changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import axios from "axios";
 
-export default function Login() {
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (isLoading) return;
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/login/", {
+        email,
+        password,
+      });
+      console.log("Login success!", response.data);
+      setIsLoggedIn(true);
+      setError(null);
+    } catch (error) {
+      if (error.response && error.response.data) {
+        const errorMessage = Object.values(error.response.data)
+          .flat()
+          .join(", ");
+        setError(errorMessage);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+      setIsLoggedIn(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="/logo.png" /> Log-in to your account
+        </Header>
+        <Form size="large" onSubmit={handleLogin}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              color="teal"
+              fluid
+              size="large"
+              loading={isLoading}
+              disabled={isLoading}
+            >
+              Login
+            </Button>
+            {error && <Message error>{error}</Message>}
+            {isLoggedIn && <Message success>Login Successful!</Message>}
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href="#">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   );
-}
+};
+
+export default LoginForm;
